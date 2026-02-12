ec9761bc5673a02e5e8ec794d67623eb0e89da0a
Beamlak E <b.d.eskender@wust.edu>
2026-02-12 13:49:05 -0600

    Second attempt

--- gradingScript/grading.sh
+++ gradingScript/grading.sh
@@ -1,11 +1,27 @@
 #!/bin/bash 
 
-let = $output;
-output = key;
+i dot know howo to do this soo ill try framing it in a sense//
 
-while(somethinng is reading studentkeys){
-   String = https://github.com/CSE2307SP26/assignment-3-beamlak-e.git; 
 
+
+let = $output; 
+output = key; 
+
+
+while(output is reading student keys){ 
+   if (something)
+    keys something 
+
+  { then(----){ 
+    String = https://github.com/CSE2307SP26/assignment-3-beamlak-e.git;
+    
+  if(output exists){
+    if(mathces){
+
+    return score 1}
+  } elif score 0
+}
+)
 
 }
 
